75f7c10c8b9758a70f85ff09f65f16c902c1761f
SamZhang02 <sam.zijing.zhang@gmail.com>
2022-12-01 22:29:04 -0500

    initialize

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}')
@@ -26,11 +25,11 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.author.id == 1038921977865457804:
+        await message.channel.send('i am the better bot')
+
     if message.content.startswith('!hello'):
-        if message.author == 'wugway#7417':
-            await message.channel.send('tg simon')
-        else:
-            await message.channel.send('I am Ally\'s unfinished project bot')
+        await message.channel.send('I am Ally\'s unfinished project bot')
     
     if message.content == ('test image'):
         await message.channel.send('https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg')
